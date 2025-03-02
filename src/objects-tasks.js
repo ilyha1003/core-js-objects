b3dd2a400e2aef38b4246feb49410be919018208
Sankovich Ilya <ilyhasankovich@gmail.com>
2025-03-02 18:13:52 +0300

    feat: solve some tasks from object-tasks.js

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -78,8 +78,8 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -93,8 +93,12 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  if (Object.keys(obj).length < 1) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
